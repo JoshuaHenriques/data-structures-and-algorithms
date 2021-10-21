9a7caf6c3a3d5aaae7ff1e260d857711d3795125
joshuahenriques <joshuahenriques95@gmail.com>
2021-10-20 20:17:35 -0400

    create urlify.py

---
+++ cracking-the-coding-interview/interview-questions/arrays-and-strings/urlify.py
@@ -0,0 +1,11 @@
+'''
+#3 Write a method to replace all spaces in a string with '%20'. You many assume
+that the string has sufficent space at the end to hold the additional
+characters, and that you are given the "true" length of the string. (Notes: If
+implementing in Java, please use a character array so that you can perform this
+operation in place.) 
+
+Examples:
+Input:		"Mr John Smith		", 13
+Output:		"Mr%20John%20Smith"
+'''
