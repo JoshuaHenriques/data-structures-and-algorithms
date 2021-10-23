a864d22972d9d572157d71559a67b0cfa937bf66
joshuahenriques <joshuahenriques95@gmail.com>
2021-10-22 22:36:07 -0400

    create one_away.py

---
+++ cracking-the-coding-interview/interview-questions/arrays-and-strings/one_away.py
@@ -0,0 +1,38 @@
+'''
+#5
+There are three types of edits that can be performed on strings: insert a
+character, remove a character, or replace a character. Given two strings, write
+a function to check if they are one edit (or zero edits) away.
+
+Examples: 
+pale,	ple		->	true
+pales,	pale	->	true
+pale,	bale	->	true
+pale,	bake	->	false
+'''
+
+def one_away(str0: str, str1: str) -> bool:
+	# Remove character
+	if len(str0) - len(str1) == 1:
+		return True
+	# Insert character
+	elif len(str0) - len(str1) == -1:
+		return True
+	# Same length, check if more than two characters are changed
+	elif len(str0) - len(str1) == 0:
+		dict = {}
+		flag = False
+		for i, char in enumerate(str0):
+			if str0[i] != str1[i]:
+				if flag == True:
+					return False
+				flag = True
+		return True
+	else:
+		return False
+
+if __name__ == "__main__":
+	print(one_away("pale", "ple"))
+	print(one_away("pales", "pale"))
+	print(one_away("pale", "bale"))
+	print(one_away("pale", "bake"))
