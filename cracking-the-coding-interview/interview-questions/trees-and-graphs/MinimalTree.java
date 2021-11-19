e49d44db418eb5ca4699f4ecd716fdc260a2f4f1
joshuahenriques <joshuahenriques95@gmail.com>
2021-11-19 16:30:26 -0500

    commit

---
+++ cracking-the-coding-interview/interview-questions/trees-and-graphs/MinimalTree.java
@@ -0,0 +1,34 @@
+/*
+Given a sorted (increasing order) array with unique integer elements, write an
+algorithm to create a binary search tree with minimal height.
+
+Input: [3, 5, 6, 7, 9, 14]
+Output: 
+
+Hints: 
+19. A minimal binary tree has about the same number of nodes on the left of each
+    node as on the right. Let's focus on just the root for now. How would you
+    ensure that about the same number of nodes are on the left of the root as
+   	on the right? 
+
+73. You could implement this by finding the "ideal" next element to add and
+    repeatedly calling insertValue. This will be a bit inefficient, as you
+    would have to repeatedly traverse the tree. Try recursion instead. Can you
+    divide this problem into subproblems?
+
+116. Imagine we had a createMinimalTree method that returns a minimal tree for a
+     given array (but for some strange reason doesn't operate on the root of the
+     tree). Could you use this to operate on the root of the tree? Could you
+     write the base case for the function? Great? Thien that's basically the
+     entire function.
+*/
+public class MinimalTree {
+	
+	public static void createMinimalBST() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
